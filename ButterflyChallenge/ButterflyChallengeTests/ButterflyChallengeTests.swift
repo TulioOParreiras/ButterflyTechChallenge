cea5026f358231da7912127982574263a12e047c
Tulio Parreiras <tulio_parreiras@hotmail.com>
2024-03-01 12:39:04 +1100

    Create empty project

---
+++ ButterflyChallenge/ButterflyChallengeTests/ButterflyChallengeTests.swift
@@ -0,0 +1,11 @@
+//
+//  ButterflyChallengeTests.swift
+//  ButterflyChallengeTests
+//
+//  Created by Tulio Parreiras on 1/3/2024.
+//
+
+import XCTest
+@testable import ButterflyChallenge
+
+final class ButterflyChallengeTests: XCTestCase { }
---
+++ ButterflyChallenge/ButterflyChallengeUITests/ButterflyChallengeUITests.swift
@@ -0,0 +1,10 @@
+//
+//  ButterflyChallengeUITests.swift
+//  ButterflyChallengeUITests
+//
+//  Created by Tulio Parreiras on 1/3/2024.
+//
+
+import XCTest
+
+final class ButterflyChallengeUITests: XCTestCase { }
