306707c28f997b23d0a8aa5442a1c65d46a01f3c
Tulio Parreiras <tulio_parreiras@hotmail.com>
2024-03-05 17:12:30 +1100

    Show loading indicator while fetching movie details

--- ButterflyChallenge/ButterflyChallenge/MovieDetails/MovieDetailsView.swift
+++ ButterflyChallenge/ButterflyChallenge/MovieDetails/MovieDetailsView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+struct MovieDetails {
+    let id: String
+    let title: String
+    let posterImageURL: URL?
+    let releaseDate: String
+    let overview: String
+    let originalTitle: String
+    let duration: String
+}
+
 protocol MovieDetailsLoader {
-    typealias LoadResult = Result<Movie, Error>
+    typealias LoadResult = Result<MovieDetails, Error>
     
     func loadMovieData(from movie: Movie, completion: @escaping (LoadResult) -> Void)
+}
+
+enum MovieDetailsViewState {
+    case loading
+    case loaded(MovieDetails)
+    case failure(Error)
+    case empty
 }
 
 final class MovieDetailsViewModel: ObservableObject {
     let movie: Movie
     let movieDetailsLoader: MovieDetailsLoader
+    
+    @Published var viewState: MovieDetailsViewState = .empty
     
     init(movie: Movie, movieDetailsLoader: MovieDetailsLoader) {
         self.movie = movie
@@ -23,19 +42,44 @@ final class MovieDetailsViewModel: ObservableObject {
     }
     
     func onViewLoad() {
-        movieDetailsLoader.loadMovieData(from: movie, completion: { _ in })
+        viewState = .loading
+        movieDetailsLoader.loadMovieData(from: movie, completion: { [weak self] result in
+            do {
+                let value = try result.get()
+                self?.viewState = .loaded(value)
+            } catch {
+                self?.viewState = .failure(error)
+            }
+        })
     }
 }
 
 struct MovieDetailsView: View {
+    enum ViewIdentifiers: String {
+        case loading = "loading-indicator"
+    }
     
     @ObservedObject var viewModel: MovieDetailsViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        content
             .onAppear {
                 viewModel.onViewLoad()
             }
+    }
+    
+    @ViewBuilder
+    var content: some View {
+        switch viewModel.viewState {
+        case .loading:
+            ProgressView().accessibilityIdentifier(ViewIdentifiers.loading.rawValue)
+        case .loaded(let movie):
+            EmptyView()
+        case .failure(let error):
+            EmptyView()
+        case .empty:
+            EmptyView()
+        }
     }
 }
 
@@ -49,6 +93,20 @@ class MovieDetailsLoaderMock: MovieDetailsLoader {
 extension Movie {
     static var mock: Movie {
         Movie(id: "1", title: "A title", posterImageURL: nil, releaseDate: "A date")
+    }
+}
+
+extension MovieDetails {
+    static var mock: MovieDetails {
+        MovieDetails(
+            id: "1",
+            title: "A title",
+            posterImageURL: nil,
+            releaseDate: "A date",
+            overview: "An overview",
+            originalTitle: "A title",
+            duration: "2h 30m"
+        )
     }
 }
 #endif
--- ButterflyChallenge/ButterflyChallengeTests/MovieDetailsIntegrationTests.swift
+++ ButterflyChallenge/ButterflyChallengeTests/MovieDetailsIntegrationTests.swift
@@ -13,9 +13,20 @@ import ViewInspector
 
 class MovieLoaderSpy: MovieDetailsLoader {
     var requestedMovie: Movie?
+    private var movieLoadCompletion: ((LoadResult) -> Void)?
     
     func loadMovieData(from movie: Movie, completion: @escaping (LoadResult) -> Void) {
+        movieLoadCompletion = completion
         requestedMovie = movie
+    }
+    
+    func completeLoading(with movieDetails: MovieDetails) {
+        movieLoadCompletion?(.success(movieDetails))
+    }
+    
+    func completeLoadingWithFailure() {
+        let error = NSError(domain: "", code: 0)
+        movieLoadCompletion?(.failure(error))
     }
 }
 
@@ -27,7 +38,66 @@ final class MovieDetailsIntegrationTests: XCTestCase {
         let viewModel = MovieDetailsViewModel(movie: movie, movieDetailsLoader: loader)
         let view = MovieDetailsView(viewModel: viewModel)
         ViewHosting.host(view: view)
-        XCTAssertEqual(movie, loader.requestedMovie)
+        XCTAssertEqual(movie, loader.requestedMovie, "Expected to request load details from movie assigned to View Model")
+    }
+    
+    func test_loadingMovieIndicator_isVisibleWhileLoadingMovieDetails() {
+        let movie = Movie(id: "1", title: "A title", posterImageURL: nil, releaseDate: "A date")
+        let loader = MovieLoaderSpy()
+        let viewModel = MovieDetailsViewModel(movie: movie, movieDetailsLoader: loader)
+        let view = MovieDetailsView(viewModel: viewModel)
+        XCTAssertFalse(view.isShowingLoadingIndicator, "Expected to not show loading indicator until view appears")
+        
+        ViewHosting.host(view: view)
+        XCTAssertTrue(view.isShowingLoadingIndicator, "Expected to show loading indicator once view appears")
+        
+        loader.completeLoading(with: makeMovieDetails())
+        XCTAssertFalse(view.isShowingLoadingIndicator, "Expected to hide loading indicator when details are loaded with success")
+    }
+    
+    func test_loadingMovieIndicator_isNotVisibleAfterDetailsLoadFailure() {
+        let movie = Movie(id: "1", title: "A title", posterImageURL: nil, releaseDate: "A date")
+        let loader = MovieLoaderSpy()
+        let viewModel = MovieDetailsViewModel(movie: movie, movieDetailsLoader: loader)
+        let view = MovieDetailsView(viewModel: viewModel)
+        XCTAssertFalse(view.isShowingLoadingIndicator, "Expected to not show loading indicator until view appears")
+        
+        ViewHosting.host(view: view)
+        XCTAssertTrue(view.isShowingLoadingIndicator, "Expected to show loading indicator once view appears")
+        
+        loader.completeLoadingWithFailure()
+        XCTAssertFalse(view.isShowingLoadingIndicator, "Expected to hide loading indicator when receiving a failure on details load")
+    }
+    
+    // MARK: - Helpers
+    
+    func makeMovieDetails(
+        id: String = "1",
+        title: String = "A title",
+        posterImageURL: URL? = nil,
+        releaseDate: String = "A date",
+        overview: String = "Movie overview",
+        originalTitle: String = "The original title",
+        duration: String = "Some duration"
+    ) -> MovieDetails {
+        MovieDetails(
+            id: id,
+            title: title,
+            posterImageURL: posterImageURL,
+            releaseDate: releaseDate,
+            overview: overview,
+            originalTitle: originalTitle,
+            duration: duration
+        )
     }
 
+}
+
+extension MovieDetailsView {
+    
+    var isShowingLoadingIndicator: Bool {
+        let view = try? inspect().find(viewWithAccessibilityIdentifier: MovieDetailsView.ViewIdentifiers.loading.rawValue)
+        return view != nil
+    }
+    
 }
